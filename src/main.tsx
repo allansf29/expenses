8e475ebbec82b205749f51f7e29ed22a47359e05
Allan Victor <allansf29@gmail.com>
2025-09-29 21:32:55 -0300

    Removendo alguns componentes.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import Home from './pages/Home.tsx'
 import Navbar from './components/ui/Navbar.tsx'
-import CategotyAnalysis from '../src/pages/CategoryAnalysis.tsx'
 import Dashboard from './pages/Dashboard.tsx'
 import Calendar from './components/Calendar.tsx'
 
@@ -16,7 +15,6 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/category-analysis" element={<CategotyAnalysis />} />
         <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </BrowserRouter>
